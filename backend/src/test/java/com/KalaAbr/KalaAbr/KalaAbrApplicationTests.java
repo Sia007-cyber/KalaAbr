b2d56e21bab78a4780dd74a21ab5dc8d3eb3a152
Alireza Nourbakhsh <3nderblack@gmail.com>
2026-07-28 19:21:22 +0330

    back  1st commit

---
+++ backend/src/test/java/com/KalaAbr/KalaAbr/KalaAbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KalaAbr.KalaAbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalaAbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
