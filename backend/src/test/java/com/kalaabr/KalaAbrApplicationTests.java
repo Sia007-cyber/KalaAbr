e234824d084a70a0d61b59ad0feddd374c94a66b
Alireza Nourbakhsh <3nderblack@gmail.com>
2026-08-04 13:24:51 +0330

    chi shode boud?

--- backend/src/test/java/com/kalaabr/KalaAbrApplicationTests.java
+++ backend/src/test/java/com/kalaabr/KalaAbrApplicationTests.java
@@ -3,6 +3,11 @@ package com.kalaabr;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * تست اسموک بارگذاری کانتکست اسپرینگ.
+ * برای اجرای این تست، Postgres محلی باید طبق application.yml بالا و در دسترس باشد
+ * (دیتابیس kalaabr، یوزر/پسورد kalaabr روی localhost:5432).
+ */
 @SpringBootTest
 class KalaAbrApplicationTests {
 
